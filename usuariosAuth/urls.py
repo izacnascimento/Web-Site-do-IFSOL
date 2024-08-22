77893b61e5725f593bad64aba50b9e7cfa84e615
izacnascimento <izac.n@escolar.ifrn.edu.br>
2024-08-22 10:29:45 -0300

    Atualizado layoutadmin.html

--- usuariosAuth/urls.py
+++ usuariosAuth/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from info.views import superuser
-# from info.views import confirmar_compra
 
 urlpatterns = [
-    path('cadastro',views.cadastro, name='cadastro'),
-    path('login',views.login, name='login'),
-    path('logout' ,views.logout, name='logout'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('AlterarSenha', views.AlterarSenha, name='AlterarSenha'),
-    path('controle', superuser , name='controle'),
-    path('cadastrarprodutos', views.cadastrarprodutos , name='cadastrarprodutos'),
-    path('pgcadastrar', views.pgcadastrar , name='pgcadastrar'),
-    path('detalhar_pedido/<int:carrinho_id>/', views.detalhar_pedido , name='detalhar_pedido'),
-    # path('confirmar_compra/<int:carrinho_id>/', views.confirmar_compra, name='confirmar_compra'),
-
+    path('controle', superuser, name='controle'),
+    path('cadastrarprodutos', views.cadastrarprodutos, name='cadastrarprodutos'),
+    path('pgcadastrar', views.pgcadastrar, name='pgcadastrar'),
+    path('detalhar_pedido/<int:carrinho_id>/', views.detalhar_pedido, name='detalhar_pedido'),
+    path('editar_produto/<int:produto_id>/', views.editar_produto, name='editar_produto'),
+    path('excluir_produto/<int:produto_id>/', views.excluir_produto, name='excluir_produto'),
+    path('confirmar_compra/<int:carrinho_id>/', views.confirmar_compra, name='confirmar_compra'),
+    path('pagina_de_erro/<int:carrinho_id>/', views.pagina_de_erro, name='pagina_de_erro'),
 ]
--- usuariosAuth/views.py
+++ usuariosAuth/views.py
@@ -1,19 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
-from info.models import Usuario
-from info.models import Produtos
-from info.models import Carrinho
-from info.models import ItemCarrinho
+from info.models import Usuario, Produtos, Carrinho
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login
 
-def cadastro (request):
+def cadastro(request):
     if request.method == 'POST':
-        
         nome = request.POST['inputName']
         sobrenome = request.POST['inputSurname']
         email = request.POST['inputEmail4']
@@ -32,118 +26,92 @@ def cadastro (request):
         if senha != repetir_senha:
             data['msg'] = 'Senha e Confirma Senha diferentes!'
             data['class'] = 'alert-danger'
-            return render (request,'usuarios/cadastro.html',data)
+            return render(request, 'usuarios/cadastro.html', data)
         else:
-            authuser = User.objects.create_user(username=nome, last_name=sobrenome, email=email,password=senha)
+            authuser = User.objects.create_user(username=nome, last_name=sobrenome, email=email, password=senha)
             user = Usuario.objects.create(chave=authuser, cpf=cpf, telefone=telefone, tipo_publico=tipo_publico, endereco=endereco, numero_casa=numero_casa, complemento=complemento, cep=cep, cidade=cidade, estado=estado)
             authuser.save()
             user.save()
-            return redirect ('login')
-    
+            return redirect('login')
     else:
-        return render (request, 'usuarios/cadastro.html')
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
     if request.method == "POST":
         email = request.POST['inputEmail4']
         senha = request.POST['inputPassword4']
-
-        # Verifique se o usuário com o email existe
         if User.objects.filter(email=email).exists():
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
-            
-            # Tente autenticar o usuário com o nome de usuário e senha
             user = authenticate(request, username=nome, password=senha)
-            
             if user is not None:
                 auth_login(request, user)
-                return redirect('index')  # Redireciona para a página inicial.
+                return redirect('index')
             else:
                 messages.error(request, "Senha inválida!")
         else:
             messages.error(request, "Email não encontrado!")
-        
-        return redirect('login')  # Redireciona de volta para a página de login
-
+        return redirect('login')
     return render(request, 'usuarios/login.html')
-             
-def logout (request):
+
+def logout(request):
     auth.logout(request)
-    return render (request, 'index.html')
+    return render(request, 'index.html')
 
-def AlterarSenha (request):
-
+def AlterarSenha(request):
     if request.user.is_authenticated:
         if request.method == "POST":
             senha = request.POST['inputPassword4']
             repetir_senha = request.POST['inputrepsenha']
             data = {}
-        
             if senha != repetir_senha:
                 data['msg'] = 'Senha e Confirma Senha diferentes!'
                 data['class'] = 'alert-danger'
-                return render (request, 'usuarios/alterar_senha.html', data)
-            
+                return render(request, 'usuarios/alterar_senha.html', data)
             else:
                 novasenha = User.objects.get(pk=request.user.id)
                 novasenha.set_password(senha)
                 novasenha.save()
-                
                 return redirect('index')
-            
         else:    
-            return render (request, 'usuarios/alterar_senha.html')
-        
-        
+            return render(request, 'usuarios/alterar_senha.html')
     else:
-        return render (request, 'index.html')
-  
-def cadastrarprodutos (request):
+        return render(request, 'index.html')
+
+def cadastrarprodutos(request):
     if request.method == 'POST':
-       
         nome = request.POST.get('nomeproduto')
         preco = request.POST.get('precoproduto')
         unidade_de_medida = request.POST.get('unidadeproduto')
         imagem = request.FILES.get('imagemproduto')
         data = {}
-        
-        # Verifica se os valores foram recebidos corretamente
         if nome and preco and unidade_de_medida:
             if imagem:
                 produto = Produtos.objects.create(nome=nome, preco=preco, unidade_de_medida=unidade_de_medida, imagem=imagem)
             else:
                 produto = Produtos.objects.create(nome=nome, preco=preco, unidade_de_medida=unidade_de_medida)
-            # Faça a operação com os valores
-            prod = Produtos.objects.filter(nome=nome, preco=preco, unidade_de_medida=unidade_de_medida, imagem=imagem)
             produto.save()
             data['msg'] = 'Produto cadastrado com Sucesso!'
             data['class'] = 'alert-success'
-            return render (request, 'usuarios/cadastrar_produtos.html', data)
+            return render(request, 'usuarios/cadastrar_produtos.html', data)
         else:
             data['msg'] = 'Produto não cadastrado!'
             data['class'] = 'alert-danger'
-    
     else:
-        return render (request, 'usuarios/cadastrar_produtos.html')  
+        return render(request, 'usuarios/cadastrar_produtos.html')
 
-    
-def pgcadastrar (request):
-        listagem_de_produtos = Produtos.objects.all()
-        return render (request, 'usuarios/cadastrar_produtos.html', {'listagem_de_produtos':listagem_de_produtos})
+def pgcadastrar(request):
+    listagem_de_produtos = Produtos.objects.all()
+    return render(request, 'usuarios/cadastrar_produtos.html', {'listagem_de_produtos': listagem_de_produtos})
 
 def detalhar_pedido(request, carrinho_id):
     carrinho = get_object_or_404(Carrinho, pk=carrinho_id)
     itens_carrinho = carrinho.itens.all()
-    
-    # Agora você tem acesso aos itens do carrinho para este usuário
-    # e pode passá-los para o seu template para exibição
-    
     return render(request, 'usuarios/detalhar_pedido.html', {'carrinho': carrinho, 'itens_carrinho': itens_carrinho})
 
 def editar_produto(request, produto_id):
-    produto = get_object_or_404(Produto, id=produto_id)
+    produto = get_object_or_404(Produtos, id=produto_id)
     if request.method == 'POST':
-        form = ProdutoForm(request.POST, request.FILES, instance=produto)
+        form = Produtos(request.POST, request.FILES, instance=produto)
         if form.is_valid():
             form.save()
             messages.success(request, 'Produto atualizado com sucesso!')
@@ -151,26 +119,25 @@ def editar_produto(request, produto_id):
         else:
             messages.error(request, 'Erro ao atualizar o produto. Verifique os dados.')
     else:
-        form = ProdutoForm(instance=produto)
+        form = Produtos(instance=produto)
     return render(request, 'editar_produto.html', {'form': form, 'produto': produto})
 
 def excluir_produto(request, produto_id):
-    produto = get_object_or_404(Produto, id=produto_id)
+    produto = get_object_or_404(Produtos, id=produto_id)
     if request.method == 'POST':
         produto.delete()
         messages.success(request, 'Produto excluído com sucesso!')
         return redirect('listar_produtos')
     return render(request, 'confirmar_exclusao.html', {'produto': produto})
 
+def confirmar_compra(request, carrinho_id):
+    if request.user.is_superuser:
+        carrinho = get_object_or_404(Carrinho, pk=carrinho_id)
+        carrinho.confirmado = True
+        carrinho.save()
+        return redirect('pagina_de_confirmacao')
+    
 
-
-# @login_required
-# def confirmar_compra(request, carrinho_id):
-#     if request.user.is_superuser:
-#         carrinho = get_object_or_404(Carrinho, pk=carrinho_id)
-#         carrinho.confirmado = True
-#         carrinho.save()
-#         return redirect('pagina_de_confirmacao')  # Redirecione para a página de confirmação ou outra página desejada após a confirmação
-#     else:
-#         # Se o usuário não for um superusuário, talvez você queira redirecioná-lo para outra página ou exibir uma mensagem de erro
-#         return redirect('pagina_de_erro')
+def pagina_de_erro(request, carrinho_id=None):
+    # lógica para a página de erro
+    return render(request, 'pagina_de_erro.html')
